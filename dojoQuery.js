1aba6acc39077e2bc22d4e079874dbd95318a5e4
Vince Carini <vincecarini@northwesternmutual.com>
2018-01-12 08:26:20 -0600

    everything you know is wrong

--- dojoQuery.js
+++ dojoQuery.js
@@ -1,18 +1,18 @@
 (function (global) {
 
-  function myAccessorFunction(elementId) {
-    let htmlElement = document.createElement("html");
-    
-    htmlElement.click = function (callback) {
+  function myAccessorFunction() {
+    let dojoElement = document.createElement("dojo");
+
+    dojoElement.click = function (callback) {
       callback()
     }
-    htmlElement.hover = function (hoverOn, hoverOff) {
+    dojoElement.hover = function (hoverOn, hoverOff) {
       if(typeof hoverOff != 'function')
         hoverOn()
       else
         hoverOff()
     }
-    return htmlElement;
+    return dojoElement;
 
   }
 
